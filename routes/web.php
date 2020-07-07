ca5d8e4f4298b1ce92916b7635d3c8baeb7b38ba
oscar <oscar_sr_609@hotmail.com>
2020-07-06 23:24:30 -0500

    Primero commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/paypal/pay', 'PaymentController@payWithPayPal');
+Route::get('/paypal/status', 'PaymentController@payPalStatus');
+Route::get('/paypal/results', 'PaymentController@results')->name('paypal.results');
+Route::get('/paypal/failed', 'PaymentController@failed')->name('paypal.failed');
+
+
